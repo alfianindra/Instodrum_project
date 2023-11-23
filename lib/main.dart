d6947bcde9a364b0bceb463027ec15399a0619c9
MaryAntonietteVincent <asenzmaryanto@gmail.com>
2023-11-23 11:38:13 +0700

    Tes

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo 1',
       theme: ThemeData(
 
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
