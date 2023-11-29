080e794853fb845610e68ec3293a64b7e5d402fe
Hansen <hansenpratama947@gmail.com>
2023-11-29 15:14:32 +0700

    Penambahan search page

---
+++ lib/pages/search.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatefulWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  final List<Map<String, dynamic>> _allUsers = [
+    {"id": 1, "name": "Alfian Indrajaya", "age": 29},
+    {"id": 2, "name": "Novandry Aprilian", "age": 40},
+    {"id": 3, "name": "Carlouis Fernando", "age": 5},
+    {"id": 4, "name": "Maryanto", "age": 35},
+    {"id": 5, "name": "Hansen Pratama", "age": 21},
+    {"id": 6, "name": "Witriatna Kosahi", "age": 55},
+    {"id": 7, "name": "Merry Anthony", "age": 30},
+    {"id": 8, "name": "Sarah", "age": 14},
+    {"id": 9, "name": "Briliona", "age": 100},
+    {"id": 10, "name": "Gilang", "age": 32},
+  ];
+
+  List<Map<String, dynamic>> _foundUsers = [];
+  @override
+  initState() {
+    _foundUsers = _allUsers;
+    super.initState();
+  }
+
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyword.isEmpty) {
+      results = _allUsers;
+    } else {
+      results = _allUsers
+          .where((user) =>
+              user["name"].toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+    }
+
+    setState(() {
+      _foundUsers = results;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              onChanged: (value) => _runFilter(value),
+              decoration: const InputDecoration(
+                  labelText: 'Search', suffixIcon: Icon(Icons.search)),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: _foundUsers.isNotEmpty
+                  ? ListView.builder(
+                      itemCount: _foundUsers.length,
+                      itemBuilder: (context, index) => Card(
+                        key: ValueKey(_foundUsers[index]["id"]),
+                        color: Colors.amberAccent,
+                        elevation: 4,
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        child: ListTile(
+                          leading: Text(
+                            _foundUsers[index]["id"].toString(),
+                            style: const TextStyle(fontSize: 24),
+                          ),
+                          title: Text(_foundUsers[index]['name']),
+                          subtitle: Text(
+                              '${_foundUsers[index]["age"].toString()} years old'),
+                        ),
+                      ),
+                    )
+                  : const Text(
+                      'No results found',
+                      style: TextStyle(fontSize: 24),
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
