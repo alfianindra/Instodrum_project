b067a2cd2a595e313dc7eb22bb1df729dae0fa3f
Hansen <hansenpratama947@gmail.com>
2023-11-29 15:16:46 +0700

    Search page

---
+++ lib/homepage.dart
@@ -0,0 +1,44 @@
+import 'package:instodrum/pages/home.dart';
+import 'package:flutter/material.dart';
+import 'package:instodrum/pages/search.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) :super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _selectedIndex = 0;
+  void _navigateBottomNavBar (int Index) {
+    setState(() {
+      _selectedIndex = Index;
+    });
+  }
+
+//buat halaman masing-masing
+final List<Widget> _children = [
+UserHome(),
+Search(),
+];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( //kerangka 
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomNavBar,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search),label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_call),label: 'reels'),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Profile'),
+          ],    
+      ),
+    ); 
+  }
+}
