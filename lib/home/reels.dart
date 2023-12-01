7f2670d958ba0460573af64f077353d346d33f7e
alfianindra <alfianij8@gmail.com>
2023-12-01 12:37:45 +0700

    Route dan perbaikan dari merge witriatna

--- lib/home/reels.dart
+++ lib/home/reels.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:instodrum/util/reels_detail.dart';
 import 'package:instodrum/util/reels_pages.dart';
 
-void main() => runApp(UserReels());
-
 class UserReels extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -24,64 +22,62 @@ class UserReels extends StatelessWidget {
             ),
           ],
         ),
-        body: 
-        PageView.builder(
-          scrollDirection: Axis.vertical,
-          itemCount: 6,
-          itemBuilder: (context, index){
-          return Container(
-            decoration: BoxDecoration( 
-              border: Border.all(color: Colors.black),
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                  'https://picsum.photos/id/${index + 1047}/800/1080'))),
-            child: Center(
-              child: Stack(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Colors.black.withOpacity(0.3), 
-                          Colors.transparent],
-                        begin: Alignment(0, -0.75),
-                        end: Alignment(0, 0.1),
-                        )),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Colors.black.withOpacity(0.3), 
-                          Colors.transparent],
-                        end: Alignment(0, -0.75),
-                        begin: Alignment(0, 0.1),
-                        )),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
+        body: PageView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            'https://picsum.photos/id/${index + 1047}/800/1080'))),
+                child: Center(
+                  child: Stack(
                     children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
+                      Container(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                          colors: [
+                            Colors.black.withOpacity(0.3),
+                            Colors.transparent
+                          ],
+                          begin: Alignment(0, -0.75),
+                          end: Alignment(0, 0.1),
+                        )),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                          colors: [
+                            Colors.black.withOpacity(0.3),
+                            Colors.transparent
+                          ],
+                          end: Alignment(0, -0.75),
+                          begin: Alignment(0, 0.1),
+                        )),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                         Flexible(
-                          flex: 14,
-                           child: ReelsDetail()
-                         ),
-                          Flexible(
-                            flex: 2,
-                            child: ReelsPages(),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Flexible(flex: 14, child: ReelsDetail()),
+                              Flexible(
+                                flex: 2,
+                                child: ReelsPages(),
+                              ),
+                            ],
                           ),
                         ],
-                      ),
+                      )
                     ],
-                  )
-                ],
-              ),            
-            ),
-          );
-        }),
+                  ),
+                ),
+              );
+            }),
       ),
     );
   }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,46 +1,46 @@
+import 'package:flutter/material.dart';
 import 'package:instodrum/home/home.dart';
-import 'package:flutter/material.dart';
-import 'package:instodrum/pages/home.dart';
-import 'package:flutter/material.dart';
 import 'package:instodrum/pages/search.dart';
+import 'package:instodrum/home/reels.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) :super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   int _selectedIndex = 0;
-  void _navigateBottomNavBar (int Index) {
+  void _navigateBottomNavBar(int Index) {
     setState(() {
       _selectedIndex = Index;
     });
   }
 
 //buat halaman masing-masing
-final List<Widget> _children = [
-UserHome(),
-Search(),
-];
+  final List<Widget> _children = [
+    UserHome(),
+    Search(),
+    UserReels(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( //kerangka 
+    return Scaffold(
+      //kerangka
       body: _children[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _navigateBottomNavBar,
         type: BottomNavigationBarType.fixed,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search),label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.video_call),label: 'reels'),
-          BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Profile'),
-          ],    
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_call), label: 'reels'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
       ),
-    ); 
+    );
   }
 }
